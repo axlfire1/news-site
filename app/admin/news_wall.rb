830b5f7922eb46159a5e3090e13ba99bf4f5682f
David Sarmiento <edst83@gmail.com>
2023-12-14 20:58:04 -0600

    fix sections to be erased

--- app/admin/news_wall.rb
+++ app/admin/news_wall.rb
@@ -1,6 +1,11 @@
 ActiveAdmin.register NewsWall do
   permit_params :slogan, :title, :subtitle, :author, :content, :section_id
 
+  filter :email
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
+
   index do
     selectable_column
     id_column
@@ -14,11 +19,6 @@ ActiveAdmin.register NewsWall do
     actions
   end
 
-  filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
-
   form do |f|
     f.inputs do
       f.input :slogan
@@ -30,5 +30,30 @@ ActiveAdmin.register NewsWall do
     end
 
     f.actions
+  end
+
+  member_action :delete_news_wall_element, method: :delete do
+    if resource.destroy
+      redirect_to admin_news_walls_path, notice: 'Show element deleted successfully.'
+    else
+      redirect_to admin_news_walls_path, alert: 'Failed to delete show element.'
+    end
+  end
+
+  show do
+    attributes_table do
+      row :slogan
+      row :title
+      row :subtitle
+      row :author
+      row :content
+      row :section do |row|
+        row.section&.name
+      end
+    end
+
+    div do
+      button_to 'Delete', delete_news_wall_element_path, method: :delete, data: { confirm: 'Are you sure?' }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check  
+
+  # custom active admin admin
+  delete '/admin/sections/:id/delete_element', to: 'admin/sections#delete_element', as: :delete_element
+  delete '/admin/news_walls/:id/delete_element', to: 'admin/news_walls#delete_news_wall_element', as: :delete_news_wall_element
 end
