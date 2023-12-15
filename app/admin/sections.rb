b7b4ecd1387c58ed34de2ec39b791c97edb5c22a
David Sarmiento <edst83@gmail.com>
2023-12-14 20:24:08 -0600

    add delete on sections

--- app/admin/sections.rb
+++ app/admin/sections.rb
@@ -1,6 +1,23 @@
 ActiveAdmin.register Section do
+  actions :all, except: [:destroy]
+
   permit_params :name
 
-  def index
+  member_action :delete_element, method: :delete do
+    if resource.destroy
+      redirect_to delete_element_path, notice: 'Show element deleted successfully.'
+    else
+      redirect_to section_path, alert: 'Failed to delete show element.'
+    end
+  end
+
+  show do
+    attributes_table do
+      row :name
+    end
+
+    div do
+      button_to 'Delete Show Element', section_path(resource), method: :delete, data: { confirm: 'Are you sure?' }
+    end
   end
 end
