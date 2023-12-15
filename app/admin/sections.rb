9dec56467cd82bbc749e61c4076a9f910205d06a
David Sarmiento <edst83@gmail.com>
2023-12-14 20:46:28 -0600

    fix delete sections

--- app/admin/sections.rb
+++ app/admin/sections.rb
@@ -5,9 +5,9 @@ ActiveAdmin.register Section do
 
   member_action :delete_element, method: :delete do
     if resource.destroy
-      redirect_to delete_element_path, notice: 'Show element deleted successfully.'
+      redirect_to admin_sections_path, notice: 'Show element deleted successfully.'
     else
-      redirect_to section_path, alert: 'Failed to delete show element.'
+      redirect_to admin_sections_path, alert: 'Failed to delete show element.'
     end
   end
 
@@ -17,7 +17,7 @@ ActiveAdmin.register Section do
     end
 
     div do
-      button_to 'Delete Show Element', section_path(resource), method: :delete, data: { confirm: 'Are you sure?' }
+      button_to 'Delete', delete_element_path, method: :delete, data: { confirm: 'Are you sure?' }
     end
   end
 end
