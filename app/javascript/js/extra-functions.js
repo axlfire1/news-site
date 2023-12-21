f9ffbdf470283cf553f4f8d4dc2928b163610bb8
David Sarmiento <edst83@gmail.com>
2023-12-20 21:25:59 -0600

    re arrange javascript files

---
+++ app/javascript/js/extra-functions.js
@@ -0,0 +1,14 @@
+selectnav("nav", {
+  label: "-Navigation-",
+  nested: true,
+  indent: "-",
+});
+selectnav("f_menu", {
+  label: "-Navigation-",
+  nested: true,
+  indent: "-",
+});
+$(".bxslider").bxSlider({
+  mode: "fade",
+  captions: true,
+});
